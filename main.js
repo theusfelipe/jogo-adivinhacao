4f88c4e5781389e854f5d2d4170600e80d404f52
Matheus Felipe <matheusibnf@hotmail.com>
2024-06-18 15:26:21 -0300

    putting events to the buttons projects

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
-const randomNumber = Math.round(Math.random() * 10)
-let xAttempts = 1
+const randomNumber = Math.round(Math.random() * 10);
+let xAttempts = 1;
 
+//função callback
+function handleTryClick(event) {
+  event.preventDefault();
 
-function handleClick(event) {
-  event.preventDefault()
-
-  const inputNumber = document.querySelector("#inputNumber")
+  const inputNumber = document.querySelector("#inputNumber");
 
   if (Number(inputNumber.value) == randomNumber) {
-   document.querySelector (".screen1").classList.add("hide")
-   document.querySelector (".screen2").classList.remove("hide")
+    document.querySelector(".screen1").classList.add("hide");
+    document.querySelector(".screen2").classList.remove("hide");
 
-   document.querySelector(".screen2 h2").innerText = `Acertou em ${xAttempts} tentativas`
-
+    document.querySelector(
+      ".screen2 h2"
+    ).innerText = `Acertou em ${xAttempts} tentativas`;
   }
 
-  xAttempts++
+  xAttempts++;
 }
+
+//Eventos
+const btnTry = document.querySelector("#btnTry");
+const btnReset = document.querySelector("#btnReset");
+
+btnTry.addEventListener("click", handleTryClick);
