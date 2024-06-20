36109f8245b92e2b4713ba924e4d23d5fd89f8b5
Matheus Felipe <matheusibnf@hotmail.com>
2024-06-20 17:06:10 -0300

    refatoring the code, to learn

--- main.js
+++ main.js
@@ -2,18 +2,27 @@ const screen1 = document.querySelector(".screen1");
 const screen2 = document.querySelector(".screen2");
 const btnTry = document.querySelector("#btnTry"); //Eventos
 const btnReset = document.querySelector("#btnReset"); //Eventos
-const randomNumber = Math.round(Math.random() * 10);
+let randomNumber = Math.round(Math.random() * 10);
 let xAttempts = 1;
 
-//função callback
+//Eventos
+btnTry.addEventListener("click", handleTryClick);
+btnReset.addEventListener("click", handleResetClick);
+document.addEventListener("keydown", function (e) {
+
+  if (e.key == "Enter" && screen1.classList.cointains('hide')) {
+    handleResetClick();
+  }
+});
+
+//funções callback
 function handleTryClick(event) {
   event.preventDefault();
 
   const inputNumber = document.querySelector("#inputNumber");
 
   if (Number(inputNumber.value) == randomNumber) {
-    screen1.classList.add("hide");
-    screen2.classList.remove("hide");
+    toggleScreen();
 
     screen2.querySelector(
       "h2"
@@ -23,9 +32,13 @@ function handleTryClick(event) {
   xAttempts++;
 }
 
-btnTry.addEventListener("click", handleTryClick);
-btnReset.addEventListener("click", function () {
-  screen1.classList.remove("hide");
-  screen2.classList.add("hide");
+function handleResetClick() {
+  toggleScreen();
   xAttempts = 1;
-});
+  randomNumber = Math.round(Math.random() * 10);
+}
+
+function toggleScreen() {
+  screen1.classList.toggle("hide");
+  screen2.classList.toggle("hide");
+}
